a02dc5e2b5b7b9aaa84ec6d6dd1470d5a4bc32c6
UralSultan <uralbeksultan@gmail.com>
2026-07-04 17:32:41 +0600

    Добавил модель для поста обсуждения, назвал Topic. переопределил метод get_absolute_url

--- discussions/models.py
+++ discussions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from forum import settings
+from django.urls import reverse
 
-# Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='topics')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('discussions:topic-detail', kwargs={'pk': self.pk})
