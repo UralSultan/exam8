651c1547cb28ceb3d95ab3db9008908609163bbf
UralSultan <uralbeksultan@gmail.com>
2026-07-04 17:34:35 +0600

    Добавил модель для ответа Reply

--- discussions/models.py
+++ discussions/models.py
@@ -16,3 +16,16 @@ class Topic(models.Model):
 
     def get_absolute_url(self):
         return reverse('discussions:topic-detail', kwargs={'pk': self.pk})
+
+
+class Reply(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='replies')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='replies')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.text
