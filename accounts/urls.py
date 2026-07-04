107932f74801fd05d1ebf51b66f3d5ae7644cef4
UralSultan <uralbeksultan@gmail.com>
2026-07-04 21:14:24 +0600

    Добавил представление для профиля пользователя UserProfileView, использовал генерик DetailView, импортировал класс Count для ведения подсчета ответов.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserProfileView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/<int:pk>/', UserProfileView.as_view(), name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import login
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
-
+from django.views.generic import CreateView, DetailView
+from django.db.models import Count
 from accounts.forms import RegisterForm
+from . models import User
+from discussions.models import Topic
 
 
 class RegisterView(CreateView):
@@ -14,3 +16,23 @@ class RegisterView(CreateView):
         response = super().form_valid(form)
         login(self.request, self.object)
         return response
+
+
+class UserProfileView(DetailView):
+    model = User
+    template_name = 'accounts/profile.html'
+    context_object_name = 'profile_user'
+    pk_url_kwarg = 'pk'
+
+    def get_queryset(self):
+        return User.objects.annotate(replies_count=Count('replies'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topics'] = (
+            Topic.objects
+            .filter(author=self.object)
+            .annotate(replies_count=Count('replies'))
+            .order_by('-created_at')
+        )
+        return context
